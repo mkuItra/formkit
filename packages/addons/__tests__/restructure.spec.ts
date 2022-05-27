5fef9269a51e8c5f802891554e6734801c76b52f
Justin Schroeder <justin@formkit.com>
2022-05-27 11:54:36 -0400

    Adds some addition schema destructuring, but pausing work here

--- packages/addons/__tests__/restructure.spec.ts
+++ packages/addons/__tests__/restructure.spec.ts
@@ -1,23 +1,116 @@
-import {
-  FormKitPlugin,
-  FormKitSchemaCondition,
-  FormKitSchemaNode,
-} from '@formkit/core'
+import { createMaps } from '../src/plugins/restructure'
 
-type Schema = FormKitSchemaCondition | FormKitSchemaNode[]
-
-/**
- * Rebuilds a new schema from the original by using meta.section, meta.parent,
- * and meta.index to determine the correct position of the schema.
- * @param schema - The schema to restructure
- * @returns
- */
-function rebuild(schema: Schema): Schema {
-  return schema
-}
-
-export const restructure: FormKitPlugin = function (node) {
-  node.hook.schema((schema, next) => {
-    return rebuild(next(schema))
+describe('createMaps', () => {
+  it('should create a node map', () => {
+    const schema = [
+      {
+        $el: 'div',
+        meta: {
+          section: 'goodSection',
+        },
+      },
+      {
+        $cmp: 'SuperComponent',
+        meta: {
+          index: 0,
+        },
+      },
+    ]
+    const nodeMap = new Map()
+    const parentMap = new Map()
+    const sectionMap = new Map()
+    nodeMap.set(schema[0], {
+      naturalParent: null,
+      naturalIndex: 0,
+      explicitParent: undefined,
+      explicitIndex: undefined,
+    })
+    nodeMap.set(schema[1], {
+      naturalParent: null,
+      naturalIndex: 1,
+      explicitParent: undefined,
+      explicitIndex: 0,
+    })
+    parentMap.set(null, new Set(...[schema]))
+    sectionMap.set('goodSection', new Set([schema[0]]))
+    expect(
+      createMaps(schema, [new Map(), new Map(), new Map()], null, 0)
+    ).toEqual([nodeMap, parentMap, sectionMap])
   })
-}
+
+  it('creates a node map with depth', () => {
+    const schema = [
+      {
+        $el: 'div',
+        meta: {
+          section: 'goodSection',
+        },
+        children: [
+          {
+            $el: 'h1',
+            children: 'Hello world',
+            meta: {
+              section: 'title',
+            },
+          },
+        ],
+      },
+      {
+        $cmp: 'SuperComponent',
+        meta: {
+          index: 0,
+        },
+      },
+    ]
+    const nodeMap = new Map()
+    const parentMap = new Map()
+    const sectionMap = new Map()
+    nodeMap.set(schema[0], {
+      naturalParent: null,
+      naturalIndex: 0,
+      explicitParent: undefined,
+      explicitIndex: undefined,
+    })
+    nodeMap.set(schema[0].children![0], {
+      naturalParent: schema[0],
+      naturalIndex: 0,
+      explicitParent: undefined,
+      explicitIndex: undefined,
+    })
+    nodeMap.set(
+      { $el: 'text', children: 'Hello world' },
+      {
+        naturalParent: schema[0].children![0],
+        naturalIndex: 0,
+        explicitParent: undefined,
+        explicitIndex: undefined,
+      }
+    )
+    nodeMap.set(schema[1], {
+      naturalParent: null,
+      naturalIndex: 1,
+      explicitParent: undefined,
+      explicitIndex: 0,
+    })
+    parentMap.set(
+      schema[0],
+      new Set(
+        ...[
+          Object.assign(schema[0].children, {
+            children: { $el: 'text', children: 'Hello world' },
+          }),
+        ]
+      )
+    )
+    parentMap.set(
+      schema[0].children![0],
+      new Set([{ $el: 'text', children: 'Hello world' }])
+    )
+    parentMap.set(null, new Set(...[schema]))
+    sectionMap.set('goodSection', new Set([schema[0]]))
+    sectionMap.set('title', new Set([schema[0].children![0]]))
+    expect(
+      createMaps(schema, [new Map(), new Map(), new Map()], null, 0)
+    ).toEqual([nodeMap, parentMap, sectionMap])
+  })
+})
--- packages/addons/src/plugins/restructure.ts
+++ packages/addons/src/plugins/restructure.ts
@@ -1,3 +1,142 @@
-export function restructure(): void {
-  // Will do something here
+import {
+  FormKitPlugin,
+  FormKitSchemaCondition,
+  FormKitSchemaNode,
+  isComponent,
+  isConditional,
+  // isConditional,
+  isDOM,
+} from '@formkit/core'
+
+type Schema = FormKitSchemaNode[] | FormKitSchemaNode | undefined
+
+type ParentMap = Map<FormKitSchemaNode | null, Set<FormKitSchemaNode>>
+
+type SectionMap = Map<string | number | null, Set<FormKitSchemaNode>>
+
+type NodeMap = Map<
+  FormKitSchemaNode | null,
+  {
+    naturalParent: FormKitSchemaNode | null
+    naturalIndex: number
+    explicitParent: string | undefined
+    explicitIndex: number | undefined
+  }
+>
+
+declare module '@formkit/core' {
+  export interface FormKitSchemaMeta {
+    section?: string
+    parent?: string | number | null
+  }
+}
+
+export function createMaps(
+  node: Schema,
+  maps: [NodeMap, ParentMap, SectionMap],
+  naturalParent: FormKitSchemaNode | null,
+  naturalIndex: number
+): [NodeMap, ParentMap, SectionMap] {
+  if (node === undefined) return maps
+  if (Array.isArray(node)) {
+    node.forEach((child, index) =>
+      createMaps(child, maps, naturalParent, index)
+    )
+  } else {
+    const [nodeMap, parentMap, sectionMap] = maps
+    if (isDOM(node) || isComponent(node)) {
+      const explicitParent =
+        typeof node.meta?.parent === 'string' ? node.meta.parent : undefined
+      const explicitIndex =
+        node.meta && isFinite(Number(node.meta.index))
+          ? Number(node.meta.index)
+          : undefined
+      nodeMap.set(node, {
+        naturalParent,
+        naturalIndex,
+        explicitParent,
+        explicitIndex,
+      })
+      if (
+        typeof node.meta?.section === 'string' ||
+        typeof node.meta?.section === 'number'
+      ) {
+        sectionMap.set(
+          node.meta.section,
+          (sectionMap.get(node.meta.section) || new Set()).add(node)
+        )
+      }
+      if (node.children) {
+        createMaps(node.children, maps, node, 0)
+      }
+    } else if (typeof node === 'string') {
+      node = {
+        $el: 'text',
+        children: node,
+      }
+      nodeMap.set(node, {
+        naturalParent,
+        naturalIndex,
+        explicitParent: undefined,
+        explicitIndex: undefined,
+      })
+    }
+    parentMap.set(
+      naturalParent,
+      (parentMap.get(naturalParent) || new Set()).add(node)
+    )
+  }
+  return maps
+}
+
+function createChildren(
+  parent: null | FormKitSchemaNode,
+  maps: [NodeMap, ParentMap, SectionMap]
+): FormKitSchemaNode[] {
+  const [nodeMap, parentMap, sectionMap] = maps
+  const nodes: FormKitSchemaNode[] = []
+  const nodeSet = parentMap.get(parent)
+  const parentData = nodeMap.get(parent)
+  if (nodeSet === undefined) return nodes
+  nodeSet.forEach((node) => {
+    const nodeData = nodeMap.get(node)
+    if (nodeData?.explicitParent === undefined) {
+      nodes.push(nodeData)
+    }
+  })
+  return nodes
+}
+
+/**
+ * Rebuilds a new schema from the original by using meta.section, meta.parent,
+ * and meta.index to determine the correct position of the schema.
+ * @param schema - The schema to restructure
+ * @returns
+ */
+function rebuild<
+  T extends FormKitSchemaCondition | FormKitSchemaNode[] | undefined
+>(schema: T, maps: [NodeMap, ParentMap, SectionMap]): T {
+  const [, parentMap] = maps
+  if (schema === undefined || schema === null) return schema
+  const newSchema = Array.isArray(schema)
+    ? createChildren(null, maps)
+    : parentMap.get(null)?.values().next().value
+  return newSchema
+}
+
+export const restructure: FormKitPlugin = function (node) {
+  node.hook.schema(
+    (
+      schema: FormKitSchemaCondition | FormKitSchemaNode[] | undefined,
+      next
+    ) => {
+      const maps = createMaps(
+        schema,
+        [new Map(), new Map(), new Map()],
+        null,
+        0
+      )
+      return rebuild(next(schema), maps)
+    }
+  )
 }
